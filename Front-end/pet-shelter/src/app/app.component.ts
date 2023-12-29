62483bf91dd9953674d16a6c46295d48a67c08f6
Youssef Aboelwafa <96186143+YoussefAboelwafa@users.noreply.github.com>
2023-12-30 00:56:45 +0200

    .

--- Front-end/pet-shelter/src/app/app.component.ts
+++ Front-end/pet-shelter/src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent {
   title = 'pet-shelter';
   constructor(private router: Router) {}
   ngOnInit() {
-    this.router.navigate(['/signup']);
+    this.router.navigate(['/staffpage/manageadoption']);
   }
 }
